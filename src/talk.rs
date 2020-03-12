4d98dd3439f23a5b2a6ee1029176b068d286ed16
Kamil Koczurek <koczurekk@gmail.com>
2020-03-12 16:41:17 +0100

    send bytes in chunks to minimize synchronization overhead

--- src/talk.rs
+++ src/talk.rs
@@ -6,31 +6,33 @@ use crate::frame::*;
 use crate::Config;
 
 /* play sounds corresponding to incoming bytes */
-fn play(receiver: mpsc::Receiver<Option<u8>>, conf: Config) {
+fn play(receiver: mpsc::Receiver<Option<Vec<u8>>>, conf: Config) {
     let mut fb = FrameBuilder::new(conf.band);
 
     let device = rodio::default_output_device().unwrap();
     let sink = Sink::new(&device);
 
     for maybe in receiver.iter() {
-        let b = match maybe {
-            Some(byte) => byte,
+        let bytes = match maybe {
+            Some(v) => v,
             None => break,
         };
-        fb.build(false, b, &sink, conf.clk_low_time);
-        fb.build(true, b, &sink, conf.clk_high_time);
+        for b in bytes {
+            fb.build(false, b, &sink, conf.clk_low_time);
+            fb.build(true, b, &sink, conf.clk_high_time);
+        }
     }
 
     sink.sleep_until_end();
 }
 
 /* read bytes from stding and send them to be played */
-fn stdin_reader(sender: mpsc::Sender<Option<u8>>) {
+fn stdin_reader(sender: mpsc::Sender<Option<Vec<u8>>>) {
     use std::io::Read;
 
     let stdin = std::io::stdin();
     let mut handle = stdin.lock();
-    let mut buf = [0];
+    let mut buf = [0; 256];
 
     loop {
         let cnt = match handle.read(&mut buf) {
@@ -38,9 +40,8 @@ fn stdin_reader(sender: mpsc::Sender<Option<u8>>) {
             Ok(n) => n,
         };
 
-        for k in 0 .. cnt {
-            sender.send(Some(buf[k])).unwrap();
-        }
+        let bytes = buf[0..cnt].into();
+        sender.send(Some(bytes)).unwrap();
     }
 
     sender.send(None).unwrap();
